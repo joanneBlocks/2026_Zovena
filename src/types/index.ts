63b18354454c9d9cc26f65cc2071c8e7895a1950
joanneBlocks <jscosto@gmail.com>
2026-04-01 00:47:06 +0800

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,12 @@ export interface Profile {
   role: Role
   created_at: string
 }
+
+export interface Pet {
+  id: string
+  name: string
+  species: string
+  age: number
+  owner_id: string
+  created_at: string
+}
